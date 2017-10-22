be6c2315169a416c4b7c5ed3f1160e9045b9e5a1
iArray-osx <akrd.kmnc@kkumail.com>
2017-10-23 00:43:48 +0700

    create dictation funtion

--- Scripy.py
+++ Scripy.py
@@ -14,6 +14,22 @@ def cls():
 	else :
 		os.system('clear')
 		#print("\033[H\033[J")
+	
+
+def print_line(x=None):
+	if x is None:
+		print "-"*40
+	else:
+		print "-"*x
+
+def print_con():
+	x=raw_input("press enter to continue.")
+
+def loading():
+	global done
+	done = False
+	t = threading.Thread(target=animate)
+	t.start()
 
 def animate():
 	global done
@@ -27,11 +43,21 @@ def animate():
 		sys.stdout.flush()
 		time.sleep(0.3)
 		
-		
+def degree(a,b):
+	x=a*1.0/b
+	if(x>=0.8):
+		return "Excellent!"
+	elif(x>=0.6):
+		return "Good"
+	elif(x>=0.4):
+		return "Fair"
+	else:
+		return "Try harder"
 
 def random_word():
 	global vocab
 	global word_vocab
+	word_vocab=[]
 	while len(word_vocab)<10:
 		tmp=random.choice(vocab).lower()
 		if tmp not in word_vocab:
@@ -51,12 +77,47 @@ def get_translate():
 
 def dictation():
 	global word_vocab
+	global trans_vocab1
+	global trans_vocab2
+	tmp=['','']
+	ran=[0,1,2,3,4]
+	random.shuffle(ran)
+	score=0
+	
+	for i in ran:
+		if(lan[0]!=''):
+			tmp[0]= '[%s] %s'%(lan[0],trans_vocab1[i])
+		if(lan[1]!=''):
+			tmp[1]= '[%s] %s'%(lan[1],trans_vocab2[i])
+		cls()
+		print "--------Dictaion--------"
+		print "Did you remember this?\n"
+		print "%s %s\n"%(tmp[0],tmp[1])
+		j=0
+		while(j<3):
+			x=raw_input("In english is: ")
+			if(x.lower()==word_vocab[i]):
+				score+=1
+				print "----- Correct! -----\n"
+				print_con()
+				break
+			else:
+				print "----- Try again! -----\n"
+			j+=1
+		if(j==3):
+			print_line()
+			print "%s %s in english is %s"%(tmp[0],tmp[1],word_vocab[i])
+			print_line()
+			print_con()
 
-def loading():
-	global done
-	done = False
-	t = threading.Thread(target=animate)
-	t.start()
+	cls()
+	print "--------Dictaion--------"
+	print_line(24)
+	print "%10d/%d %s"%(score,5,degree(score,5))
+	print_line(24)
+	print_con()
+	return score
+
 
 def learn_vocab():
 	global word_vocab
@@ -64,23 +125,28 @@ def learn_vocab():
 	global trans_vocab1
 	global trans_vocab2
 	global done
-	cls()
-	print "--------Learn station--------\n"
-	print "How much that you can remember?(# Exit)\n"
-	x=raw_input("press enter to start")
-	cls()
-	print "--------Learn station--------\n"
-	print "How much that you can remember?(# Exit)\n"
-	loading()
-	#print "LOADING PLEASE WAIT..."
-	random_word()
-	get_translate()
-	done=True
-	while(x!='#'):
+	total = 0
+	score = 0
+	
+	x='y'
+	while(x=='yes' or x=='y'):
+		cls()
+		print "--------Learn station--------\n"
+		print "How much that you can remember?(# Exit)\n"
+		x=raw_input("press enter to start")
+		if(x=='#'):
+			break
+		cls()
+		print "--------Learn station--------\n"
+		print "How much that you can remember?(# Exit)\n"
+		loading()
+		random_word()
+		get_translate()
+		done=True
 		cls()
 		print "--------Learn station--------\n"
 		print "How much that you can remember?(# Exit)"
-		print "-----------------------------------------"
+		print_line()
 		tmp=['','','']
 		for i in range(5):
 			tmp[0]='[%s] %s'%('en',word_vocab[i])
@@ -89,9 +155,17 @@ def learn_vocab():
 			if(lan[1]!=''):
 				tmp[2]= '[%s] %s'%(lan[1],trans_vocab2[i])
 			print "%-15s%-15s\t%s"%(tmp[0],tmp[1],tmp[2])
-			#print "\n"
-		print "-----------------------------------------"
+		print_line()
 		x=raw_input("press enter to continue")
+		if(x=='#'):
+			break
+		total+=5
+		score+=dictation()
+		cls()
+		print "--------Learn station-------3333-\n"
+		print "total: %d \tyour score: %d \t%s\n"%(total,score,degree(score,total))
+		print "Do you want to play again?"
+		x=raw_input("yes/y or enter to exit : ")
 
 
 def setting():
@@ -134,8 +208,11 @@ def home():
 	print "Good bye!"		
 
 home()
-
 '''
+random_word()
+get_translate()
+dictation()
+
 random_word()
 get_translate()
 for i in word_vocab:
