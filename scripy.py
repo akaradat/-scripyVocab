fbdd4dc6f82f466d8a581433226931c58e0f2d2c
iArray-osx <akrd.kmnc@kkumail.com>
2017-11-15 14:14:30 +0700

    hangman with 2 player and fix loading vocab

--- scripy.py
+++ scripy.py
@@ -4,9 +4,9 @@ from bs4 import BeautifulSoup
 #textname='vocabularyTest.txt'
 #vocab=open(textname).read().split()
 
-url="http://www.manythings.org/vocabulary/lists/a/words.php?f=animals_1"
-page=requests.get(url)
-soup = BeautifulSoup(page.content,'html.parser')
+#url="http://www.manythings.org/vocabulary/lists/a/words.php?f=animals_1"
+#page=requests.get(url)
+#soup = BeautifulSoup(page.content,'html.parser')
 
 
 catalog_now="Animals"
@@ -31,7 +31,7 @@ def interrupted(signum, frame):
 
 def print_line(x=None):
 	if x is None:
-		print "-"*40
+		print "-"*30
 	else:
 		print "-"*x
 
@@ -70,6 +70,7 @@ def getvocab(x):
 	global done
 	#find url of catalog
 
+	vocab=[]
 	page=requests.get("http://www.manythings.org/vocabulary/lists/a/")
 	soup = BeautifulSoup(page.content,'html.parser')
 	url="http://www.manythings.org/vocabulary/lists/a/"+soup.find_all('a')[x]['href']
@@ -78,31 +79,32 @@ def getvocab(x):
 	page=requests.get(url)
 	soup = BeautifulSoup(page.content,'html.parser')
 	for x in soup.find_all('li'):
-		vocab.append(x.text)
+		vocab.append(x.text.lower())
 
 
 def get_translate():
-	global done
+	
 	global trans_vocab1
 	global trans_vocab2
 	global lan
+	global word_vocab
 	trans_vocab1=[]
 	trans_vocab2=[]
-	
-	loading()
 
-	for i in range(5):
+	for i in range(len(word_vocab)):
 		url1='https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl=%s&dt=t&q=%s'%(lan[0],word_vocab[i])
 		url2='https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl=%s&dt=t&q=%s'%(lan[1],word_vocab[i])
 		trans_vocab1.append(json.loads(requests.get(url1).text)[0][0][0].lower())
 		trans_vocab2.append(json.loads(requests.get(url2).text)[0][0][0].lower())
 	
-	done = True
+	
 
 def random_word(n):
+	global done
 	global vocab
 	global word_vocab
 	word_vocab=[]
+	loading()
 	if len(vocab) < n:
 			getvocab(catalog_selected)
 	
@@ -114,11 +116,13 @@ def random_word(n):
 				break
 
 	get_translate()
+	done = True
 
 def delword():
 	global vocab
 	global word_vocab
 	for i in word_vocab:
+		print i
 		vocab.remove(i)
 
 
@@ -129,7 +133,7 @@ def print_word_translate():
 	global trans_vocab2
 	tmp=['','','']
 	print_line()
-	for i in range(5):
+	for i in range(len(word_vocab)):
 		tmp[0]='[%s] %s'%('en',word_vocab[i])
 		if(lan[0]!=''):
 			tmp[1]= '[%s] %s'%(lan[0],trans_vocab1[i])
@@ -153,7 +157,7 @@ def dictation():
 		if(lan[1]!=''):
 			tmp[1]= '[%s] %s'%(lan[1],trans_vocab2[i])
 		cls()
-		print "--------Dictaion--------"
+		print ' Dictaion '.center(30,'-')
 		print "Did you remember this?\n"
 		print "%s %s\n"%(tmp[0],tmp[1])
 		j=0
@@ -161,11 +165,11 @@ def dictation():
 			x=raw_input("In english is: ")
 			if(x.lower()==word_vocab[i]):
 				score+=1
-				print "----- Correct! -----\n"
+				print " Correct! ".center(30,'-'),'\n'
 				print_con()
 				break
 			else:
-				print "----- Try again! -----\n"
+				print " Try again! ".center(30,'-'),'\n'
 			j+=1
 		if(j==3):
 			print_line()
@@ -174,10 +178,10 @@ def dictation():
 			print_con()
 
 	cls()
-	print "--------Dictaion--------"
-	print_line(24)
+	print " Dictaion ".center(30,'-')
+	print_line(30)
 	print "%7d/%d %s"%(score,5,degree(score,5))
-	print_line(24)
+	print_line(30)
 	print_con()
 	return score
 
@@ -193,96 +197,216 @@ def learn_vocab():
 	x='y'
 	while(x=='yes' or x=='y'):
 		cls()
-		print "--------Learn station--------\n"
-		print "How much that you can remember?(# Exit)\n"
+		print " Learn station ".center(30,'-'),'\n'
+		print "How much that you can remember?\n"
 		x=raw_input("press enter to start")
 		cls()
-		print "--------Learn station--------\n"
-		print "How much that you can remember?(# Exit)"
+		print " Learn station ".center(30,'-'),'\n'
+		print "How much that you can remember?"
 		random_word(5)
 		
 		cls()
-		print "--------Learn station--------\n"
-		print "How much that you can remember?(# Exit)"
+		print " Learn station ".center(30,'-'),'\n'
+		print "How much that you can remember?"
 		
 		print_word_translate()
 		x=raw_input("press enter to continue")
 		total+=5
 		score+=dictation()
 		cls()
-		print "--------Learn station--------\n"
+		print " Learn station ".center(30,'-'),'\n'
 		print "total: %d \tyour score: %d \t%s\n"%(total,score,degree(score,total))
 		while x!= 'n' and x!= 'y':
 			x=raw_input("Do you want to play again? (y/n) : ").lower()
+		
 		delword()
 		
+def hangman():
+	global word_vocab
+	global vocab
+	x='y'
+	while(x=='yes' or x=='y'):
+		signal.signal(signal.SIGALRM, interrupted)
+		cls()
+		print " Hangman ".center(30,'-'),'\n'
+		print "We hint all vowel in the word.\nJust guess other character.\n"
+
+		while True:
+			x=raw_input("Number of player(1/2) : ").lower()
+			if x== '1' or x== '2':
+				break
+
+		x=int(x)
+		
+		random_word(5)
+		score=[0,0]
+		usedtime=[0,0]
+		for n in range(x):
+			tmp = []
+			count=[]
+			p=[]
+			for i in range(5):
+				tmp.append(list('_'*len(word_vocab[i])))
+				count.append(0)
+				p.append(0)
+			for i in range(5):
+				for j in range(len(word_vocab[i])):
+					z=word_vocab[i][j]
+					if(z=='a' or z=='e' or z=='i' or z=='o' or z=='u'):
+						tmp[i][j]=word_vocab[i][j]
+			cls()
+			print " Hangman ".center(30,'-'),'\n'
+			z=raw_input("Player%d are you ready?\n(press enter to start)"%(n+1))
+			usedtime[n]=time.time()
+
+			try:
+				signal.alarm(60)
+				for i in itertools.cycle(range(5)):
+					if(0 not in p):
+						break
+					elif(p[i]==1):
+						continue
+					while(count[i]<5):
+						cls()
+						print " Hangman ".center(30,'-'),'\n'
+						print "Score: %d Word#: %d (# to skip.)\n"%(p.count(1),i+1)
+						print "lift left: ",5-count[i]
+						print ' '.join(tmp[i])
+						guess=raw_input("guess: ")
+						if guess == '#':
+							break
+						for k in range(len(guess)):
+							check=0
+							for j in range(len(word_vocab[i])):
+								if(guess[k] == word_vocab[i][j]):
+									tmp[i][j]=word_vocab[i][j]
+									check=1
+							if(check==0):
+								count[i]+=1
+						if(''.join(tmp[i])==word_vocab[i]):
+							p[i]=1
+							print '\n'," Correct! ".center(30,'-')
+							print 'Answer is',word_vocab[i],'\n'
+							print_con()
+							break
+						if(count[i]>=5):
+							p[i]=-1
+							print '\n',' Wrong! '.center(30,'-')
+							print 'Answer is',word_vocab[i],'\n'
+							print_con()
+
+				signal.alarm(0)
+				usedtime[n]=int(time.time()-usedtime[n])
+			except:
+				print '\n\n'," Time out! ".center(30,'-')
+				print_con()
+				usedtime[n]='timeout'
+
+
+			score[n]=p.count(1)
+			cls()
+			print " Hangman ".center(30,'-'),'\n'
+			print "Score %d/5 %s Used time: %s"%(score[n],degree(score[n],5),str(usedtime[n]))
+			print_word_translate()
+			print ''
+			print_con()
+		
+		cls()
+		print " Hangman ".center(30,'-'),'\n'
+		if(x==1):
+			print ""
+		elif(score[0]>score[1]):
+			print "The winner is player 1\n"
+		elif(score[1]>score[0]):
+			print "The winner is player 2\n"
+		elif(usedtime[0]==usedtime[1]):
+			print "Draw!"
+		elif(usedtime[0]<usedtime[1]):
+			print "The winner is player 1\n"
+		else:
+			print "The winner is player 2\n"
+		
+
+		while True:
+			x=raw_input("Do you want to play again? (y/n) : ").lower()
+			if x== 'n' or x== 'y':
+				break
+		delword()
+
+
+
+
+
+
+
+
+
 def word_shuffle():
 	global word_vocab
+	global vocab
 	x='y'
 
 	while(x=='y'):
 		signal.signal(signal.SIGALRM, interrupted)
-		
 		cls()
-		print "--------Word shuffle-------\n"
+		print " Word shuffle ".center(30,'-'),'\n'
 		x=0
 		print "Sort the shuffle letter to word."
 		print "Time limit 60 sec and # to skip.\n"
 		while not (x=='1' or x=='2'):
 			x=raw_input("Number of player(1/2) : ")
 		x=int(x)
-		random_word(10)
-		tmp=['']*10
-		for i in range(10):
+		random_word(5)
+		tmp=['']*5
+		for i in range(5):
 			tmp[i] = ' '.join(random.sample(word_vocab[i],len(word_vocab[i])))
 			while(tmp[i].replace(' ','')==word_vocab[i]):
 				tmp[i] = ' '.join(random.sample(word_vocab[i],len(word_vocab[i])))
 
 		check=[[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
 		usedtime=[0]*x
-		total =10
+		total = 5
 		for j in range(x):
 			cls()
-			print "--------Word shuffle-------\n"
+			print " Word shuffle ".center(30,'-'),'\n'
 			z=raw_input("Player%d are you ready?\n(press enter to start)"%(j+1))
 			usedtime[j]=time.time()
 
 			try:
-				signal.alarm(30)
-				for i in itertools.cycle(range(10)):
-					if sum(check[j]) == 10:
+				signal.alarm(60)
+				for i in itertools.cycle(range(5)):
+					if sum(check[j]) == 5:
 						break
 					elif check[j][i]==1:
 						continue
 					cls()
-					print "--------Word shuffle-------\n"
+					print " Word shuffle ".center(30,'-'),'\n'
 					print "Player %d turn, press # to skip.\n"%(j+1)
-					#print "------ %s ------\n"%tmp[i]
 					print tmp[i].center(27,'-')
 					y=''
 					while(y!=word_vocab[i]):
 						y=raw_input("Input: ")
 						if(y==word_vocab[i]):
-							print "----- Correct! -----\n"
+							print " Correct! ".center(30,'-'),'\n'
 							check[j][i]=1
 							print_con()
 						if(y=='#'):
 							break
-						print "----- Try again! -----\n"
+						print " Try again! ".center(30,'-'),'\n'
 				signal.alarm(0)
 				usedtime[j]=int(time.time()-usedtime[j])
 			except:
-				print "\n\n----------- Time out! ------------"
+				print '\n\n'," Time out! ".center(30,'-')
 				print_con()
 				usedtime[j]='timeout'
 			cls()
-			print "--------Word shuffle-------\n"
-			print "player %d score: %d/10 \t%s\nUsed time : %s\n"%(j+1,sum(check[j]),degree(sum(check[j]),total),str(usedtime[j]))
+			print " Word shuffle ".center(30,'-'),'\n'
+			print "player %d score: %d/%d \t%s\nUsed time : %s\n"%(j+1,sum(check[j]),5,degree(sum(check[j]),total),str(usedtime[j]))
 			print_word_translate()
 			print "\n"
 			print_con()
 		cls()
-		print "--------Word shuffle-------\n"
+		print " Word shuffle ".center(30,'-'),'\n'
 		if(x==1):
 			print ""
 		elif(sum(check[0])>sum(check[1])):
@@ -308,10 +432,10 @@ def settinglan():
 	x=''
 	while(x!='#'):
 		cls()
-		print "--------Setting Language-------"
+		print " Setting Language ".center(30,'-')
 		print "\t[1].First language: ",lan[0]
 		print "\t[2].Second language: ",lan[1]
-		print "\t #  Exit"
+		print "\t #  Back"
 		x=raw_input("Select: ")
 
 		if x=='1':
@@ -323,7 +447,7 @@ def showvocablist():
 	page=requests.get("http://www.manythings.org/vocabulary/lists/a/")
 	soup = BeautifulSoup(page.content,'html.parser') 
 	i=1
-	print ""
+	print '\n'," vocab catalog ".center(30,'-'),'\n'
 	for x in soup.find_all('li'):
 		print "[%d] %s"%(i,x.text)
 		i=i+1
@@ -333,26 +457,27 @@ def settingvocab():
 	x=''
 	while(x!='#'):
 		cls()
-		print "-------- Setting vocab -------\n"
-		print "----------- vocab catalog -----------"
+		print " Setting vocab ".center(30,'-')
 		showvocablist()
 		print_line()
-		print "Catalog now: %s"%(catalog_now)
-		x=raw_input("Select (# to exit): ")
+		print "\nCatalog now: %s"%(catalog_now)
+		x=raw_input("Select (# to back): ")
 		if(x.isdigit()):
 			if(int(x)>=1 and int(x)<=catalog_amount):
 				catalog_select=int(x)
 				getvocab(catalog_select)
+				break
 
 
 def setting():
+	global lan
 	x=''
 	while(x!='#'):
 		cls()
-		print "-------- Setting -------"
-		print "\t[1].language"
-		print "\t[2].vocab"
-		print "\t #  Exit"
+		print " Setting ".center(30,'-')
+		print "\t[1].language [%s,%s]"%(lan[0],lan[1])
+		print "\t[2].vocab catalog [%s]"%(catalog_now)
+		print "\t #  Back"
 		x=raw_input("Select: ")
 		if x=='1':
 			settinglan()
@@ -363,7 +488,7 @@ def home():
 	x=''
 	while(x!='#'):
 		cls()
-		print "---------- Welcome to Scripy Vocab ----------"
+		print " Welcome to Scripy Vocab ".center(50,'-')
 		print "\t[1].Learn vocabuary"
 		print "\t[2].Hangman Game"
 		print "\t[3].Word shuffle Game"
@@ -380,7 +505,7 @@ def home():
 		elif (x=='4'):
 			setting()
 	cls()
-	print "\n\n\n\n\n\n================= Good bye! =================\n\n\n\n\n\n\n\n"		
+	print "\n\n\n\n\n\n",' Good bye! '.center(50,"="),"\n\n\n\n\n\n\n\n"		
 
 home()
 '''
